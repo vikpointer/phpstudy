ef1fbf2fcba48cb68bcb0316cdcce83e8405bb48
vikpointer <vikpointer@gmail>
2017-09-25 19:06:40 +0500

    Another trying to push commit
    The first lesson still...

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    // http://php.net/manual/rus
+    echo 'Hello World!!!';
+    echo '<br><b>Hello!!<b><br>';
+    echo "\u{03b2}";//Escape code символов Unicode UTF-8
+    /*
+        http://rapidtables.com/code/text/unicode-characters.htm
+    */
+    echo 'Меня зовут <b>Мальцев Виктор</b>! ';
+?>
